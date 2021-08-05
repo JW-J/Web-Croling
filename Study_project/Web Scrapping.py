3c9d3910f10694915806af042fedace0a4eb94dc
jungjoowon04 <jungjoowon04@icloud.com>
2021-08-05 17:40:19 +0900

    테스트2

--- Study_project/Web Scrapping.py
+++ Study_project/Web Scrapping.py
@@ -8,11 +8,14 @@ print("진행중")
 
 print("테스트")
 print("커밋은 그냥 로컬에 저장?")
-print("프로젝트1")
-print("프로젝트1")
-print("프로젝트1")
-print("프로젝트1")
-print("프로젝트1")
+print("프로젝트2")
+print("프로젝트2")
+print("프로젝트2")
+print("프로젝트2")
+print("프로젝트2")
+print("프로젝트2")
+print("프로젝트2")
+
 
 
 
