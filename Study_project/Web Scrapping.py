a7febb68521bed17d0badd6420804829f7db5c85
jungjoowon04 <jungjoowon04@icloud.com>
2021-08-18 17:29:25 +0900

    크롤링 샘플

--- Study_project/Web Scrapping.py
+++ Study_project/Web Scrapping.py
@@ -1,20 +1,40 @@
 import requests
+from bs4 import BeautifulSoup
 
-indeed_resul = requests.get('https://www.indeed.com/jobs?q=python&limit=50&vjk=f6294b6c85e1cb47')
+indeed_result = requests.get('https://www.indeed.com/jobs?q=python&limit=50&vjk=f6294b6c85e1cb47')
 
-print(indeed_resul)
+# html 을 indeed_soup test로 저장
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 
-print("1차 수정")
-print("2차 수정")
-print("2차 수정")
-print("2차 수정")
+# html의 텍스트에서 div 태그의 클래스명:pagination 을 찾아서 넣는다.
+# 페이지가 몇개인지 알기 위해서.
+pagination = indeed_soup.find("div", {"class": "pagination"})
+
+# 태그(div)의 클래스(pagination)을 찾아서 넣은 pagination_list에서 pages에 a태그만 추출해서 넣는다.
+links = pagination.find_all('a')
+
+# [<a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>,
+#  <a class="sister" href="http://example.com/lacie" id="link2">Lacie</a>,
+#  <a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>]
+# print(pages)
 
 
 
+# pages안에 있는 a태그 리스트에서 span 태그만 가져와서 page에넣고 이를 spans 배열에 삽입한다.
 
+# 예제1) 태그 가져오기.
+# spans 배열에 삽입.
+spans = []
+for link in links:
+    spans.append(link.find("span"))
+print(spans[0:-1])
 
+# 예제2) 문자열 가져오기
+# links 내 배열에 들어가는 끝에 문자열(next) 포함되어있어서 에러발생함.
+pages = []
+for link in links[:-1]:
+    pages.append(int(link.string))
+print(pages)
 
-
-
-
-
+# 해당 페이지의 html 을 가져와서 출력
+# print(indeed_resul.text)
