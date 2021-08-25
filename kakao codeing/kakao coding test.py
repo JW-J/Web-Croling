fcb7067fbbc872a5644684f0075aa83c01e64977
jungjoowon04 <jungjoowon04@icloud.com>
2021-08-25 13:31:15 +0900

    카카오코딩테스트 연습

--- kakao codeing/kakao coding test.py
+++ kakao codeing/kakao coding test.py
@@ -13,10 +13,13 @@ def solution(participant, completion):
 answer = solution(["mislav", "stanko", "mislav", "ana"]	, ["stanko", "ana", "mislav"])
 
 #예제1
+#["leo", "kiki", "eden"] , ["eden", "kiki"]
 print(f"{answer}는 참여자 명단에는 있지만, 완주자 명단에는 없기 때문에 완주하지 못했습니다.")
 
 #예제2
+#["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]
 print(f"{answer} 는 참여자 명단에는 있지만, 완주자 명단에는 없기 때문에 완주하지 못했습니다.")
 
 #예제3
+# ["mislav", "stanko", "mislav", "ana"]	, ["stanko", "ana", "mislav"]
 print(f"{answer} 는 참여자 명단에는 두명이 있지만 완주자 명단에는 한 명밖에 없기 때문에 한명은 완주하지 못했습니다.")
